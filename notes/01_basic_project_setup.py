60c3a3fe20f5d00f9a73513634b2140a43ecea2f
Ajay-Kumar-Maurya <ajkmaurya2015@gmail.com>
2026-08-23 18:48:38 +0530

    added dummy python file

---
+++ notes/01_basic_project_setup.py
@@ -0,0 +1,15 @@
+'''
+When you want to add new file using Github UI, you only see option to add file, not folder.
+Simply click on create file button, then you will see that a blank file will be opened.
+But you don't see any option to add folder.
+
+So, the trick is:
+  You see an input box, where you can type file name.
+  Instead of writing file name, you can simply write your folder-name and then press "/" button.
+  You will see that a new folder hierarchy is added.
+
+  If you want to delete the folder, simply click on the folder you added, it will show you pop-box.
+  Simple click on discard button.
+'''
+
+print("Hello World")
